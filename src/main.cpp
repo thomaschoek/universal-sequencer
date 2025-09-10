eeac8c2b78d994552d6ff98aa99171e29d8c27dc
Thomas Hoek <thomas.hoek@proton.me>
2025-09-10 13:40:45 +0200

    feat: simple test in main.cpp, build script

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include "sequence/clock.h"
 #include "sequence/output.h"
 
+#include <chrono>
+#include <iostream>
+
 int main() {
-    return 0;
+  using namespace MicroComposer_sequence;
+  auto clock_ = Sequence_clock();
+
+  auto t_start = std::chrono::high_resolution_clock::now();
+  clock_.start();
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  clock_.stop();
+
+  auto t_end = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Elapsed time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(t_end -
+                                                                     t_start)
+                   .count()
+            << " ms" << std::endl;
+
+  auto output_ = SequenceOutput();
+  return 0;
 }
