5298dc249e9592c525630feed99610e41f78b33a
Thomas Hoek <thomas.hoek@proton.me>
2025-09-30 10:56:10 +0200

    draft: parallel_sequencer

--- include/sequencer/parallel_sequencer.h
+++ include/sequencer/parallel_sequencer.h
@@ -1,77 +1,44 @@
 #ifndef MICRO_COMPOSER_PARALLEL_SEQUENCER_H
 #define MICRO_COMPOSER_PARALLEL_SEQUENCER_H
 
+#include "container/atomic_vector.h"
 #include "sequencer/atomic_sequencer.h"
-#include <initializer_list>
+#include <chrono>
 #include <mutex>
-#include <vector>
 
 namespace Micro_composer {
 
 namespace sequencer {
 
-template <sequencable::Sequencable_updatable T_event> class Parallel_sequencer {
+template <sequencable::Sequencable_updatable Event_t>
+class Parallel_sequencer : public container::Atomic_vector<Atomic_sequencer<Event_t>> {
 public:
-  using Sequencer = Atomic_sequencer<T_event>;
-  using Sequencer_vector = std::vector<Sequencer>;
-  using Steps = Sequencer::Base_deque;
-  using Seq_idx = Sequencer_vector::size_type;
-  using Step_idx = Sequencer::Step_idx;
-  using Handler = Sequencer::Handler;
-  using Sequence_initializer_list = Sequencer::Initializer_list;
-  using Clock = Sequencer::Clock;
-  using Time_point = Sequencer::Time_point;
-  using Step_iterator = Sequencer::Step_iterator;
+  using Sequencer = Atomic_sequencer<Event_t>;
+  using Base_vector = container::Atomic_vector<Sequencer>;
+  using Seq_idx = typename Base_vector::Index;
+  using Clock = typename Sequencer::Clock;
+  using Time_point = typename Sequencer::Time_point;
 
-  void start(Seq_idx, Time_point = Clock::now());
-  void start_all(Time_point = Clock::now());
+  // Constructors
+  Parallel_sequencer() = default;
+  Parallel_sequencer(const Parallel_sequencer&) = default;
+  Parallel_sequencer& operator=(const Parallel_sequencer&) = default;
+  Parallel_sequencer(Parallel_sequencer&&) noexcept = default;
+  ~Parallel_sequencer() = default;
+
+  // Synchronized transport control
+  void start(Seq_idx, Time_point start_time = Clock::now());
+  void start_all(Time_point start_time = Clock::now());
+
   void stop(Seq_idx);
   void stop_all();
 
-  // Sequence-level CRUD
-  void add_sequence(Sequence_initializer_list);
-  void add_sequence(Sequence_initializer_list, Handler);
-
-  const Steps& get(Seq_idx) const;
-
-  void assign(Seq_idx, std::initializer_list<T_event>);
-
-  void drop_sequence(Seq_idx);
-  void set_handler(Seq_idx, Handler);
-  Seq_idx size() const;
-  bool empty() const;
-
-  // Step-level CRUD
-  void push_back(Seq_idx, const T_event&);
-  void push_back(Seq_idx, Sequence_initializer_list);
-  void push_front(Seq_idx, const T_event&);
-  void push_front(Seq_idx, Sequence_initializer_list);
-
-  Step_idx insert(Seq_idx, Step_idx, const T_event&);
-  Step_idx insert(Seq_idx, Step_idx, T_event&&);
-
-  template <typename... Args> void update(Seq_idx, Step_idx, Args&&... args);
-  void replace(Seq_idx, Step_idx, const T_event&);
-  void replace(Seq_idx, Step_idx, T_event&&);
-
-  const T_event front(Seq_idx) const;
-  const T_event back(Seq_idx) const;
-  const T_event at(Seq_idx, Step_idx) const;
-
-  void pop_back(Seq_idx);
-  void pop_front(Seq_idx);
-
-  void erase(Seq_idx, Step_idx);
-
-  // Clear all
-  void clear();
-  void clear(Seq_idx);
+  bool is_running(Seq_idx) const;
+  bool any_running() const;
+  bool all_running() const;
 
 private:
-  std::mutex transport_mutex_;
-  std::mutex crud_mutex_;
-
-  Sequencer_vector sequencers_;
+  mutable std::mutex transport_mutex_;
 };
 
 } // namespace sequencer
---
+++ include/sequencer/parallel_sequencer.tpp
@@ -0,0 +1,84 @@
+#include "sequencer/parallel_sequencer.h"
+#include <stdexcept>
+
+namespace Micro_composer {
+
+namespace sequencer {
+
+template <sequencable::Sequencable_updatable Event_t>
+void Parallel_sequencer<Event_t>::start(Seq_idx idx, Time_point start_time) {
+  std::scoped_lock lck{transport_mutex_};
+  if (idx >= Base_vector::size()) {
+    throw std::out_of_range(
+        "[ERROR] In Parallel_sequencer::start: Index out of range.");
+  }
+  Base_vector::operator[](idx).start(start_time);
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+void Parallel_sequencer<Event_t>::start_all(Time_point start_time) {
+  std::scoped_lock lck{transport_mutex_};
+  auto count = Base_vector::size();
+  for (Seq_idx i = 0; i < count; ++i) {
+    Base_vector::operator[](i).start(start_time);
+  }
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+void Parallel_sequencer<Event_t>::stop(Seq_idx idx) {
+  std::scoped_lock lck{transport_mutex_};
+  if (idx >= Base_vector::size()) {
+    throw std::out_of_range(
+        "[ERROR] In Parallel_sequencer::stop: Index out of range.");
+  }
+  Base_vector::operator[](idx).stop();
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+void Parallel_sequencer<Event_t>::stop_all() {
+  std::scoped_lock lck{transport_mutex_};
+  auto count = Base_vector::size();
+  for (Seq_idx i = 0; i < count; ++i) {
+    Base_vector::operator[](i).stop();
+  }
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+bool Parallel_sequencer<Event_t>::is_running(Seq_idx idx) const {
+  std::scoped_lock lck{transport_mutex_};
+  if (idx >= Base_vector::size()) {
+    throw std::out_of_range(
+        "[ERROR] In Parallel_sequencer::is_running: Index out of range.");
+  }
+  return Base_vector::operator[](idx).is_running();
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+bool Parallel_sequencer<Event_t>::any_running() const {
+  std::scoped_lock lck{transport_mutex_};
+  auto count = Base_vector::size();
+  for (Seq_idx i = 0; i < count; ++i) {
+    if (Base_vector::operator[](i).is_running()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+template <sequencable::Sequencable_updatable Event_t>
+bool Parallel_sequencer<Event_t>::all_running() const {
+  std::scoped_lock lck{transport_mutex_};
+  auto count = Base_vector::size();
+  if (count == 0) {
+    return false;
+  }
+  for (Seq_idx i = 0; i < count; ++i) {
+    if (!Base_vector::operator[](i).is_running()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+} // namespace sequencer
+} // namespace Micro_composer
