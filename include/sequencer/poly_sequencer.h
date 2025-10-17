5736ffbf9d2eb3b0903ee6ffdd2455436e2e330b
Thomas Hoek <thomas.hoek@proton.me>
2025-10-17 14:34:28 +0200

    init: Poly_sequencer

---
+++ include/sequencer/poly_sequencer.h
@@ -0,0 +1,12 @@
+#ifndef MICRO_COMPOSER_POLY_SEQUENCER_H
+#define MICRO_COMPOSER_POLY_SEQUENCER_H
+
+#include "sequencer.h"
+
+namespace Micro_composer {
+
+namespace sequencer {} // namespace sequencer
+
+} // namespace Micro_composer
+
+#endif
