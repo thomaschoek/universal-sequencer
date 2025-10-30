5d353f639fd452520591c1563be0b42f332e5869
Thomas Hoek <thomas.hoek@proton.me>
2025-10-30 16:58:07 +0100

    fix(Midi): schedule note offs

--- src/main_midi.cpp
+++ src/main_midi.cpp
@@ -66,15 +66,14 @@ int main(int argc, char** argv) {
         midi_output->send_message(msgs.note_on);
 
         // Schedule note-off after duration
-        auto duration_ms =
-            std::chrono::duration_cast<std::chrono::milliseconds>(
-                event.duration);
+        auto note_off_time = event.scheduled_time + event.duration;
 
-        // Spawn a thread to send note-off after delay
-        std::thread([midi_output, note_off = msgs.note_off, duration_ms]() {
-          std::this_thread::sleep_for(duration_ms);
-          midi_output->send_message(note_off);
-        });
+        std::this_thread::sleep_until(note_off_time -
+                                      std::chrono::milliseconds(5));
+        while (std::chrono::steady_clock::now() <
+               note_off_time - std::chrono::milliseconds(2))
+          ;
+        midi_output->send_message(msgs.note_off);
       };
     };
 
